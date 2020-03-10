c8a3af1e5fc145b342f20d5c618a9a39e7aa32c6
instylefreedom <wjyang95@gmail.com>
2020-03-10 13:33:49 -0400

    branch test

--- front-end/src/Make_Post.js
+++ front-end/src/Make_Post.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Make_Post = (props) => {
 
   return (
-<div className="About">
+<div className="Header">
     <h1>Create post here</h1>
     <section className="main-content">
         <img alt="about us" src="https://image.edaily.co.kr/images/Photo/files/NP/S/2015/08/PS15081300024.jpg" />
@@ -13,6 +13,18 @@ const Make_Post = (props) => {
             DO YOU KNOW PORORO?
         </p>
     </section>
+</div>
+<div className="searchBar"> 
+
+</div>
+<div className="select_type">
+
+</div>
+<div className="post">
+
+</div>
+<div className="nav_Bar">
+    
 </div>
   );
 }
