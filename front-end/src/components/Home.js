6c5192cffaa02b9cd52aef49cf0f5ae70f153dfd
actualrockstar <mmj342@nyu.edu>
2020-03-19 16:58:53 -0400

    adding logo and components folder

---
+++ front-end/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import logo from './logo.svg';
+//import './Home.css';
+
+const Home = (props) => {
+
+  return (
+<div className="Home">
+    <h1>Welcome to Sharmony!</h1>
+    <section className="main-content">
+        <img alt="sharmony logo" src="public\Sharmony Logo.PNG" />
+        <p>
+          IS THIS WORKING OR WHAT LOLOLOLOLOLOL          
+        </p>
+    </section>
+</div>
+  );
+}
+
+export default Home;
---
+++ front-end/src/components/logIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const logInScreen = (props) => {
+    return ( 
+        <div className='logInScreen'>
+            <img alt='Sharmony Logo' src="C:\Users\Kay James\Documents\sharmony\spring-2020-toxic-flamingo\front-end\public\Sharmony Logo.PNG"/>
+
+        </div>
+            
+         );
+    
+}
+ 
+export default logInScreen;
