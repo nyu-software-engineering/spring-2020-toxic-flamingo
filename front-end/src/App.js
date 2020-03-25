558508d2974bf1209cfd6a4fcc270cc64ca2a9f0
actualrockstar <mmj342@nyu.edu>
2020-03-25 16:16:10 -0400

    save

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,40 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
 import PrimaryNav from './components/PrimaryNav';
 import Home from './components/Home';
 import makePost from './components/makePost';
 import Profile from './components/Profile';
 //import Sharmony logo from './logo.svg';
-=======
-import PrimaryNav from './PrimaryNav';
-import Home from './Home';
-import Make_Post from './Make_Post';
-import Profile from './Profile';
-import logo from './logo.svg';
->>>>>>> ba7d2e4df7ad706c3b11416925c122197c47e7b0
 import './App.css';
-import logInScreen from './components/logIn';
+import logInScreen from './components/LogIn';
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ba7d2e4df7ad706c3b11416925c122197c47e7b0
   const App = (props) => {
     return (
       <div className="container">
           <Router>
               <Switch>
-<<<<<<< HEAD
 
-                  <Route path="/components/logIn">
-                      <PrimaryNav />
-                      <logInScreen />
-                      
-                  </Route>
-  
                   <Route path="/components/Make_Post">
                       <PrimaryNav />
                       <makePost />
@@ -47,31 +28,19 @@ import logInScreen from './components/logIn';
                   </Route>
 
 
-=======
-  
-                  <Route path="/Make_Post">
-                      <PrimaryNav />
-                      <Make_Post />
-                  </Route>
-  
-  
-                  <Route path="/Profile">
-                      <PrimaryNav />
-                      <Profile />
-                  </Route>
-  
->>>>>>> ba7d2e4df7ad706c3b11416925c122197c47e7b0
                   <Route path="/">
                       <PrimaryNav />
                       <Home />
                   </Route>
-<<<<<<< HEAD
+
+                  <Route path="/components/LogIn">
+                      <PrimaryNav />
+                      <LogIn />
+                      
+                  </Route>
 
                   
 
-=======
-  
->>>>>>> ba7d2e4df7ad706c3b11416925c122197c47e7b0
               </Switch>
           </Router>
       </div>
--- front-end/src/components/logIn.js
+++ front-end/src/components/logIn.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-handleSubmit = ()
 
-const logIn = (props) => {
+signUp = () => {
+  <SignUp />
+}
+
+
+const LogIn = (props) => {
 
     return (
-  <div className="LogIn">
-      <h1>Welcome to Sharmony!</h1>
-      <section className="main-content">
-          
-          <img alt="Sharmony Logo" src="C:\Users\Kay James\Documents\sharmony\spring-2020-toxic-flamingo\front-end\public\SharmonyLogo.PNG" />
-          <form onSubmit={this.handleSubmit}>
-          <p>Username: </p>
-          <input type='text' name='username' />
-          <br/>
-          <p>Password: </p>
-          <input type='password' name='password'  />
-          <br/>
-          <br/>
-          <input type='submit' />
-          </form>
-      </section>
-  </div>
+        <div className="LogIn">
+            <h1>Welcome to Sharmony!</h1>
+            <br/>
+            <section className="main-content">
+                
+                <img alt="Sharmony Logo" src="C:\Users\Kay James\Documents\sharmony\spring-2020-toxic-flamingo\front-end\public\SharmonyLogo.PNG" />
+                <form onSubmit={this.handleSubmit}>
+                <p>Username: </p>
+                <input type='text' name='username' />
+                <p>Password: </p>
+                <input type='password' name='password'  />
+                <br/>
+                <button onClick={logIn}>Log In</button>
+                <button onClick={signUp}>Sign Up</button>
+                </form>
+            </section>
+        </div>
     );
   }
 
 
   
-  export default logIn;
+  export default LogIn;
