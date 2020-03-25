dcf05c829e0eec23d8a5b9037e94bd02ab2c9759
instylefreedom <wjyang95@gmail.com>
2020-03-25 16:33:25 -0400

    back

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
-import PrimaryNav from './components/PrimaryNav';
-import Home from './components/Home';
-import makePost from './components/makePost';
-import Profile from './components/Profile';
-//import Sharmony logo from './logo.svg';
-=======
 import PrimaryNav from './PrimaryNav';
 import Home from './Home';
 import Make_Post from './Make_Post';
 import Profile from './Profile';
 import logo from './logo.svg';
->>>>>>> 77d9667c52704910cb7b0fb591e8de6c530ff388
 import './App.css';
-import logInScreen from './components/LogIn';
+
 
 
 
@@ -23,21 +15,6 @@ import logInScreen from './components/LogIn';
       <div className="container">
           <Router>
               <Switch>
-<<<<<<< HEAD
-
-                  <Route path="/components/Make_Post">
-                      <PrimaryNav />
-                      <makePost />
-                  </Route>
-  
-  
-                  <Route path="/components/Profile">
-                      <PrimaryNav />
-                      <Profile />
-                  </Route>
-
-
-=======
   
                   <Route path="/Make_Post">
                       
@@ -51,24 +28,11 @@ import logInScreen from './components/LogIn';
                       <Profile />
                   </Route>
   
->>>>>>> 77d9667c52704910cb7b0fb591e8de6c530ff388
                   <Route path="/">
                       <PrimaryNav />
                       <Home />
                   </Route>
-<<<<<<< HEAD
-
-                  <Route path="/components/LogIn">
-                      <PrimaryNav />
-                      <LogIn />
-                      
-                  </Route>
-
-                  
-
-=======
   
->>>>>>> 77d9667c52704910cb7b0fb591e8de6c530ff388
               </Switch>
           </Router>
       </div>
--- front-end/src/components/logIn.js
+++ front-end/src/components/logIn.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 77d9667c52704910cb7b0fb591e8de6c530ff388
 
 signUp = () => {
   <SignUp />
@@ -14,25 +10,6 @@ signUp = () => {
 const LogIn = (props) => {
 
     return (
-<<<<<<< HEAD
-        <div className="LogIn">
-            <h1>Welcome to Sharmony!</h1>
-            <br/>
-            <section className="main-content">
-                
-                <img alt="Sharmony Logo" src="C:\Users\Kay James\Documents\sharmony\spring-2020-toxic-flamingo\front-end\public\SharmonyLogo.PNG" />
-                <form onSubmit={this.handleSubmit}>
-                <p>Username: </p>
-                <input type='text' name='username' />
-                <p>Password: </p>
-                <input type='password' name='password'  />
-                <br/>
-                <button onClick={logIn}>Log In</button>
-                <button onClick={signUp}>Sign Up</button>
-                </form>
-            </section>
-        </div>
-=======
   <div className="LogIn">
       <h1>Welcome to Sharmony!</h1>
       <section className="main-content">
@@ -50,15 +27,9 @@ const LogIn = (props) => {
           </form>
       </section>
   </div>
->>>>>>> 77d9667c52704910cb7b0fb591e8de6c530ff388
     );
   }
 
 
-<<<<<<< HEAD
-  
-  export default LogIn;
-=======
 
   export default logIn;
->>>>>>> 77d9667c52704910cb7b0fb591e8de6c530ff388
