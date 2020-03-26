a91327113379b0f0f631892ae28d10d14d2a90e9
wesleyyu14 <33070748+wesleyyu14@users.noreply.github.com>
2020-03-25 23:15:35 -0400

    Trophies and Achievements page

--- front-end/src/Trophies.js
+++ front-end/src/Trophies.js
@@ -1,16 +1,42 @@
-import React from 'react';
-// import logo from './logo.svg';
-//import './About.css';
+import React, {useState, useEffect, Component} from 'react';
+import axios from 'axios';
+import Trophy from './Trophy';
 
 const Trophies = (props) => {
 
+        const [data, setData] = useState([]);
+    
+        // load in posts or whatever
+        useEffect( () => {
+            //fetch data
+            axios.get("https://api.mockaroo.com/api/11bdcb60?count=10&key=06908ea0")
+            .then ((response) => {
+                setData(response.data);
+            })
+            .catch( err => {
+                console.log("ERROR!");
+                console.error(err);
+    
+                //fake backup data
+                const backupData = [
+                    {
+                        id: 1,
+                        trophy: "Harmonize",
+                        trophy_description: "Get your first Harmony!",
+                        trophy_icon: "https://i.pinimg.com/originals/5f/77/4b/5f774b20b2f212b7f9b888437a097579.jpg"
+                    }
+                ];
+                setData(backupData);
+            })
+            
+        }, []);
+    
   return (
-<div className="About">
+<div className="Trophies">
     <h1>Trophies</h1>
-    <section className="main-content">
-        <img alt="about us" src="https://i.pinimg.com/originals/5f/77/4b/5f774b20b2f212b7f9b888437a097579.jpg" />
-        
-    </section>
+            {data.map((jsonObj, i) => (
+                <Trophy key={jsonObj.id} data={jsonObj}/>
+            ))}
 </div>
   );
 }
---
+++ front-end/src/Trophy.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect, Component} from 'react';
+import './Trophy.css';
+
+const Trophy = (props) => {
+    const data = props.data;
+    return (
+        <div class="flex-container">
+            <div className="trophyBox">
+            <div className='postHeader'>
+                <img className='trophyIcon' alt='icon' src={data.trophy_icon} />
+                <h3>{data.trophy}</h3>
+            </div>
+            <div className='trophyContent'>
+                <p>{data.trophy_description}</p>
+            </div>
+            </div>
+            <div className="trophyBox">
+            <div className='postHeader'>
+                <img className='trophyIcon' alt='icon' src={data.trophy_icon} />
+                <h3>{data.trophy}</h3>
+            </div>
+            <div className='trophyContent'>
+                <p>{data.trophy_description}</p>
+            </div>
+            </div>
+            <div className="trophyBox">
+            <div className='postHeader'>
+                <img className='trophyIcon' alt='icon' src={data.trophy_icon} />
+                <h3>{data.trophy}</h3>
+            </div>
+            <div className='trophyContent'>
+                <p>{data.trophy_description}</p>
+            </div>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Trophy;
