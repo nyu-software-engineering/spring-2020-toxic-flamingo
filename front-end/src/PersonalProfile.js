ee567b0fca3945720fcfc2268a88f728eb5fedb9
actualrockstar <mmj342@nyu.edu>
2020-03-26 00:00:37 -0400

    update

--- front-end/src/PersonalProfile.js
+++ front-end/src/PersonalProfile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostPreview from './PostPreview';
 import BurgerMenu from './BurgerMenu';
+import './PersonalProfile.css';
 // import logo from './logo.svg';
 //import './About.css';
 
@@ -13,7 +14,9 @@ const PersonalProfile = (props) => {
       </div>
       <div class="flex-container">
               <img alt="Profile Pic" src="https://image.edaily.co.kr/images/Photo/files/NP/S/2015/08/PS15081300024.jpg" width="100" height="100"/>
+             
               <h1>My Name</h1>
+            
       </div>
       <br />
       <p>this is my bio. hello</p>
