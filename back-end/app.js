03bf31e7a0ebb1898430159528cd7f456b6b5c89
actualrockstar <mmj342@nyu.edu>
2020-04-12 16:34:01 -0400

    added Harmonies to front end

--- back-end/app.js
+++ back-end/app.js
@@ -3,9 +3,4 @@ const express = require("express"); // CommonJS import style!
 const app = express(); // instantiate an Express object
 // we will put some server logic here later...
 // export the express app we created to make it available to other modules
-
-app.get("/", (req, res) => {
-    res.send("Hello!");
-  });
-
 module.exports = app;
---
+++ front-end/src/Harmonies.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect, Component} from 'react';
+import axios from 'axios';
+
+const Harmonies = (props) => {
+
+        const [data, setData] = useState([]);
+    
+        // load in posts or whatever
+        useEffect( () => {
+            //fetch data
+            axios.get("https://api.mockaroo.com/api/11bdcb60?count=10&key=06908ea0")
+            .then ((response) => {
+                setData(response.data);
+            })
+            .catch( err => {
+                console.log("ERROR!");
+                console.error(err);
+    
+                //fake backup data
+                const backupData = [
+                    {
+                        id: 1,
+                        song_title: "Let It Be",
+                        artist: "The Beatles"
+                      }
+                ];
+                setData(backupData);
+            })
+            
+        }, []);
+    
+  return (
+<div className="Harmonies">
+    <h1>Harmonies</h1>
+            {data.map((jsonObj, i) => (
+                <Trophy key={jsonObj.id} data={jsonObj}/>
+            ))}
+</div>
+  );
+}
+
+export default Trophies;
