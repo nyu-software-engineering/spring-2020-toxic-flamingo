a3c6b17b32bb12c577e70d20ab182f59a515b2fb
Jack <jwh434@nyu.edu>
2020-04-21 16:04:08 -0400

    basic unit tests

--- back-end/test/test-pages.js
+++ back-end/test/test-pages.js
@@ -5,7 +5,23 @@ var request = require('request');
 it('Main page content', function(done) {
     request('http://localhost:7000/Follower' , function(error, response, body) {
         expect(response).to.not.equal(null);
-        //console.log(response);
         done();
     });
 });
+
+
+it('Load comments on a post', function(done) {
+    request('http://localhost:7000/loadComments/1', function(error, response, body) {
+        expect(response).to.have.property('artist_name');
+        
+        done();
+    });
+});
+
+it('Hashtag feed', function(done) {
+    request('http://localhost:7000/hashtagFeed/Computers', function(error, response, body) {
+        expect(response).to.not.equal(null);      
+        done();
+    });
+});
+
