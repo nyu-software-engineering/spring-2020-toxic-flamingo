fb1bca0f73c26637ba0802f7f7b0e751949dfea1
actualrockstar <mmj342@nyu.edu>
2020-04-21 16:27:55 -0400

    unit test update comment function

--- back-end/test/test-pages.js
+++ back-end/test/test-pages.js
@@ -12,7 +12,7 @@ it('Main page content', function(done) {
 
 it('Load comments on a post', function(done) {
     request('http://localhost:7000/loadComments/1', function(error, response, body) {
-        expect(response).to.have.property('artist_name');
+        expect(response).to.not.equal(null);
         done();
     });   
 });     
