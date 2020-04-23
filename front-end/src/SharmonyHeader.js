02a1f10c840ae26663f341eafc398e425b7ab62e
actualrockstar <mmj342@nyu.edu>
2020-04-23 00:49:54 -0400

    update header

--- front-end/src/SharmonyHeader.js
+++ front-end/src/SharmonyHeader.js
@@ -5,6 +5,9 @@ const SharmonyHeader = (props) => {
     return(
         <div className="header">
              <img alt="SharmonyLogoCropped.PNG" src={SharmonyLogoCropped}  />
+             <a href="/PersonalProfile" >
+                <img alt='profile pic' src="https://www.dictionary.com/e/wp-content/uploads/2018/04/kawaii.jpg" width="100" height="100" />
+             </a>
         </div>
     );
 };
