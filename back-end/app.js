67e0e8db7b72f9ca904da66313a4bd48c4263190
wesleyyu14 <33070748+wesleyyu14@users.noreply.github.com>
2020-06-10 18:47:32 -0700

    sdlfkj

--- back-end/app.js
+++ back-end/app.js
@@ -27,7 +27,7 @@ const JwtCookieComboStrategy = require('passport-jwt-cookiecombo');
 const jwtDecode = require('jwt-decode');
 
 const FRONTEND_IP = process.env.NODE_ENV === "production"? "http://64.225.7.121:3000" :"http://localhost:3000";
-
+console.log(process.env.NODE_ENV)
 const corsOptions = {
   origin: FRONTEND_IP,    // reqexp will match all prefixes
   methods: "GET,HEAD,POST,PATCH,DELETE,OPTIONS",
